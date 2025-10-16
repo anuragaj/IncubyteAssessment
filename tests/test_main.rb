f92d1cdea51a0861cf3d0605fc49d447416d8a61
Anurag Joshi <anuragajoshi@gmail.com>
2025-10-16 10:08:21 +0530

    renaming tests

--- tests/test_main.rb
+++ tests/test_main.rb
@@ -2,29 +2,29 @@ require 'minitest/autorun'
 require_relative '../main.rb'
 
 class CalculatorTest < Minitest::Test
-      def test_add_two_numbers_simple_addition
+      def test_add_two_numbers
         calculator = Calculator.new
         assert_equal 5, calculator.add('2,3')
       end
-      def test_no_numbers_empty_string
+      def test_returns_zero_for_empty_string
         calculator = Calculator.new
         assert_equal 0, calculator.add('')
       end
-      def test_add_multiple_numbers_simple_addition
+      def test_add_multiple_numbers
         calculator = Calculator.new
         assert_equal 157, calculator.add('21,37,42,57')
       end
-      def test_add_numbers_simple_addition_different_delimiter
+      def test_add_with_custom_delimiter
         calculator = Calculator.new
         assert_equal 3, calculator.add("//;\n1;2")
       end
-      def test_add_negative_numbers
+      def test_raises_on_negative_number
         calculator = Calculator.new
         assert_raises ArgumentError do
           calculator.add('2,-3,4')
         end
       end
-      def test_add_Multiple_negative_numbers
+      def test_raises_on_multiple_negative_numbers
         calculator = Calculator.new
         assert_raises ArgumentError do
           calculator.add('-2,-3,-4')
