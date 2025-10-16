9f80f5caeb3f0f2fbc45fdabf2c2be7dc5222385
Anurag Joshi <anuragajoshi@gmail.com>
2025-10-16 09:28:40 +0530

    Fix test case for adding two numbers in CalculatorTest

--- tests/test_main.rb
+++ tests/test_main.rb
@@ -4,6 +4,6 @@ require_relative '../main.rb'
 class CalculatorTest < Minitest::Test
       def test_add_two_numbers
         calculator = Calculator.new
-        assert_equal 5, calculator.add(2, 3)
+        assert_equal 5, calculator.add('2,3')
       end
 end
