6849cfcf519a4f6852fa53c04878a143d9b8fb39
Anurag Joshi <anuragajoshi@gmail.com>
2025-10-16 09:35:28 +0530

    Rename test methods for clarity and add tests for multiple numbers and empty string input

--- tests/test_main.rb
+++ tests/test_main.rb
@@ -2,8 +2,16 @@ require 'minitest/autorun'
 require_relative '../main.rb'
 
 class CalculatorTest < Minitest::Test
-      def test_add_two_numbers
+      def test_add_two_numbers_simple_addition
         calculator = Calculator.new
         assert_equal 5, calculator.add('2,3')
+      end
+      def test_no_numbers_empty_string
+        calculator = Calculator.new
+        assert_equal 0, calculator.add('')
+      end
+      def test_add_multiple_numbers_simple_addition
+        calculator = Calculator.new
+        assert_equal 157, calculator.add('21,37,42,57')
       end
 end
